40837f32ae7133b2a19cf1776fcb5a3d1b6b67e1
MKrzykow <matkrzykowski@gmail.com>
2023-09-22 21:58:35 +0200

    Clean-up

--- Pygame_visualization.py
+++ Pygame_visualization.py
@@ -1,22 +1,16 @@
-# Pygame_visualization.py
-#
-# Visualization of my brownian motion script using Pygame library.
-#
-# Changelog:
-# 03.11.2016 - script revision
-
 import sys
 
 # Libraries import
 import pygame
 import pygame.gfxdraw
-from pygame.locals import QUIT
+import pygame.locals
 
 # Import my common file
 from BM_common import Particle, Particles
 
 # Size of the window
 WIDTH, HEIGHT = 800, 600
+BLACK = (0, 0, 0)
 
 
 def draw_particle(self, screen):
@@ -27,18 +21,18 @@ def draw_particle(self, screen):
 Particle.draw_particle = draw_particle
 
 
-def draw_energy(screen, particles):
-    textSurfaceObj = fontObj.render("Energy: " + str(particles.total_energy),
-                                    True, (0, 0, 0))
+def draw_energy(screen, objs, font):
+    text_render = font.render("Energy: " + str(objs.total_energy), True,
+                                 BLACK)
     textRectObj.topleft = (0, 0)
-    screen.blit(textSurfaceObj, textRectObj)
+    screen.blit(text_render, textRectObj)
 
 
-def draw_FPS(screen):
-    textSurfaceObj = fontObj.render("FPS: " + str(round(fpsClock.get_fps(), 1)),
-                                    True, (0, 0, 0))
+def draw_FPS(screen, font):
+    text_render = font.render("FPS: " + str(round(fpsClock.get_fps(), 1)),
+                                 True, BLACK)
     textRectObj.topright = (699, 0)
-    screen.blit(textSurfaceObj, textRectObj)
+    screen.blit(text_render, textRectObj)
 
 
 if __name__ == "__main__":
@@ -48,7 +42,7 @@ if __name__ == "__main__":
     N = 5  # Number of evolution steps per frame
     fpsClock = pygame.time.Clock()  # Clock initialization
 
-    fontsize = 18
+    FONTSIZE = 18
 
     # Prepare the display
     DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
@@ -58,8 +52,8 @@ if __name__ == "__main__":
     particles = Particles(3, 5, 2, 200, 0.002)
 
     # Prepare print of the text
-    fontObj = pygame.font.Font('freesansbold.ttf', fontsize)
-    textSurfaceObj = fontObj.render('', True, (0, 0, 0))
+    fontObj = pygame.font.Font('freesansbold.ttf', FONTSIZE)
+    textSurfaceObj = fontObj.render('', True, BLACK)
     textRectObj = textSurfaceObj.get_rect()
 
     # Draw the simulation
@@ -70,14 +64,14 @@ if __name__ == "__main__":
         for item in particles.items:
             item.draw_particle(DISPLAYSURF)
 
-        draw_energy(DISPLAYSURF, particles)  # Write the energy text
-        draw_FPS(DISPLAYSURF)  # Write the FPS text
+        draw_energy(DISPLAYSURF, particles, fontObj)  # Write the energy text
+        draw_FPS(DISPLAYSURF, fontObj)  # Write the FPS text
 
         for i in range(N):
             particles.time_evolution()
 
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.locals.QUIT:
                 pygame.quit()
                 sys.exit()
 
